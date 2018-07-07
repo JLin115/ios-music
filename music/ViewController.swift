923861c46c3dadf77299b4aeb04decc8ac5a8736
林哲右 <jamesbuy218@yahoo.com.tw>
2018-07-08 00:46:53 +0800

    Initial Commit

---
+++ music/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  music
+//
+//  Created by 林哲右 on 2018/7/7.
+//  Copyright © 2018 music. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
